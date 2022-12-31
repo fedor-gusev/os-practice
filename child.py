8fbfcd32f31c7403e56009fceb50d2e809d89fe0
fedor-gusev <101652966+fedor-gusev@users.noreply.github.com>
2022-12-31 11:29:06 +0300

    Update child.py

--- child.py
+++ child.py
@@ -7,8 +7,8 @@ import random
 
 pid = os.getpid()
 ppid = os.getppid()
-print(f"[Child {pid}]: I am started. PID {pid}. Parent PID {ppid}")
+print(f"Child [{pid}]: I am started. PID {pid}. Parent PID {ppid}")
 s = sys.argv[1]
 time.sleep(int(s))
-print(f"[Child {pid}]: I am ended. PID {pid}. Parent PID {ppid}")
+print(f"Child [{pid}]: I am ended. PID {pid}. Parent PID {ppid}")
 sys.exit(random.randint(0, 1))
